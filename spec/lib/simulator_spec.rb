7614e54d8da5cc3a71122aef3fd7c86bfb74009f
Daniela Baron <baron.daniela@gmail.com>
2025-02-04 07:01:55 -0500

    more thorough testing for simulator

--- spec/lib/simulator_spec.rb
+++ spec/lib/simulator_spec.rb
@@ -4,6 +4,86 @@ require_relative "../spec_helper"
 
 RSpec.describe Simulator do
   describe "#run" do
+    context "when there are minimal balances to deplete quickly" do
+      let(:config_path) { File.join(__dir__, "..", "fixtures", "example_input_minimal.yml") }
+      let(:plan) { RetirementPlan.new(config_path) }
+      let!(:results) { described_class.new(plan).run }
+
+      it "simulation runs up to age 69" do
+        expect(results.select { |r| r[:type] == :yearly_status }.last[:age]).to eq(69)
+      end
+
+      it "verifies withdrawals for age 65" do
+        row = results.find { |r| r[:age] == 65 }
+        expect(row).to match(
+          a_hash_including(
+            age: 65,
+            rrsp_balance: be_within(1).of(46_662),
+            tfsa_balance: be_within(1).of(30_310),
+            taxable_balance: be_within(1).of(60_600),
+            note: "RRSP Drawdown",
+            type: :yearly_status
+          )
+        )
+      end
+
+      it "verifies the expected values for age 66" do
+        row = results.find { |r| r[:age] == 66 }
+        expect(row).to match(
+          a_hash_including(
+            age: 66,
+            rrsp_balance: be_within(1).of(12_990),
+            tfsa_balance: be_within(1).of(30_623),
+            taxable_balance: be_within(1).of(61_206),
+            note: "RRSP Drawdown",
+            type: :yearly_status
+          )
+        )
+      end
+
+      it "verifies the expected values for age 67" do
+        row = results.find { |r| r[:age] == 67 }
+        expect(row).to match(
+          a_hash_including(
+            age: 67,
+            rrsp_balance: be_within(1).of(13_120),
+            tfsa_balance: be_within(1).of(30_939),
+            taxable_balance: be_within(1).of(31_507),
+            note: "Taxable Drawdown",
+            type: :yearly_status
+          )
+        )
+      end
+
+      it "verifies the expected values for age 68" do
+        row = results.find { |r| r[:age] == 68 }
+        expect(row).to match(
+          a_hash_including(
+            age: 68,
+            rrsp_balance: be_within(1).of(13_251),
+            tfsa_balance: be_within(1).of(31_259),
+            taxable_balance: be_within(1).of(1_512),
+            note: "Taxable Drawdown",
+            type: :yearly_status
+          )
+        )
+      end
+
+      it "verifies the expected values for age 69" do
+        row = results.find { |r| r[:age] == 69 }
+        expect(row).to match(
+          a_hash_including(
+            age: 69,
+            rrsp_balance: be_within(1).of(13_384),
+            tfsa_balance: be_within(1).of(1_271),
+            taxable_balance: be_within(1).of(1_528),
+            note: "TFSA Drawdown",
+            type: :yearly_status
+          )
+        )
+      end
+    end
+
     context "when there is a low growth rate" do
       let(:config_path) { File.join(__dir__, "..", "fixtures", "example_input_low_growth.yml") }
       let(:plan) { RetirementPlan.new(config_path) }
