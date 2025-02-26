05323e193e4e27c2999215d55e9ff73cb3aea431
Daniela Baron <baron.daniela@gmail.com>
2025-02-26 08:12:54 -0500

    naming

--- lib/simulation/simulator.rb
+++ lib/simulation/simulator.rb
@@ -15,16 +15,12 @@ module Simulation
       (retirement_age..max_age).each do |age|
         populate_current_age(age)
         market_return = return_sequence.get_return_for_age(age)
-        # TODO: 27 naming confusion - its not exactly accounts, its an array of hashes
-        # where each hash is a "transaction" of account and withdrawal amount
-        accounts = strategy.select_accounts(market_return)
-        # temp debug
-        # print_transactions(age, accounts) # Add this line
-        break if accounts.empty?
+        account_transactions = strategy.select_accounts(market_return)
+        break if account_transactions.empty?
 
-        strategy.transact(accounts)
+        strategy.transact(account_transactions)
         strategy.apply_growth(market_return)
-        record_yearly_status(age, accounts, market_return, strategy)
+        record_yearly_status(age, account_transactions, market_return, strategy)
       end
 
       build_results
@@ -34,7 +30,7 @@ module Simulation
 
     attr_reader :app_config, :strategy, :retirement_age, :max_age, :return_sequence, :results
 
-    # temp debug
+    # call from run if need to troubleshoot
     def print_transactions(age, accounts)
       puts "Age: #{age}"
       accounts.each do |transaction|
@@ -47,14 +43,14 @@ module Simulation
       strategy.current_age = age
     end
 
-    def record_yearly_status(age, accounts, market_return, strategy)
+    def record_yearly_status(age, account_transactions, market_return, strategy)
       results << {
         age: age,
         rrsp_balance: strategy.rrsp_account.balance,
         tfsa_balance: strategy.tfsa_account.balance,
         taxable_balance: strategy.taxable_account.balance,
         cash_cushion_balance: strategy.cash_cushion.balance,
-        note: accounts.map { |acc| acc[:account].name }.join(", "),
+        note: account_transactions.map { |act| act[:account].name }.join(", "),
         cpp: strategy.cpp_used?,
         rate_of_return: market_return,
         total_balance: strategy.total_balance
