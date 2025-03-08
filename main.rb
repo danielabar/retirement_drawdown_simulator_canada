15e11c8e5f6b0a660dbdf05ccf4b2779eb74437d
Daniela Baron <baron.daniela@gmail.com>
2025-03-08 15:23:46 -0500

    restore main

--- main.rb
+++ main.rb
@@ -2,5 +2,5 @@
 
 require_relative "config/environment"
 
-# Run::AppRunner.new("inputs.yml", ARGV[0]).run
-Run::AppRunner.new("inputs_demo.yml", ARGV[0]).run
+Run::AppRunner.new("inputs.yml", ARGV[0]).run
+# Run::AppRunner.new("inputs_demo.yml", ARGV[0]).run
